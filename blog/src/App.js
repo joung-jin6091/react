47953bb5b8de6a8e99efa2e58eb6557c76e80ff9
joung <joung422@empas.com>
2025-03-25 18:41:12 +0900

    git setup first

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          안녕하세요 hello
+          안녕하세요
         </p>
         <a
           className="App-link"
